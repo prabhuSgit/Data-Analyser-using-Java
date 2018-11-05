f57f09f5b0c625040cd8f1dc874952f77acb2004
urja96 <jain.u@husky.neu.edu>
2018-11-05 14:38:18 -0500

    Urja's commit

--- Lab_8_skeleton/src/lab_8/analytics/AnalysisHelper.java
+++ Lab_8_skeleton/src/lab_8/analytics/AnalysisHelper.java
@@ -22,16 +22,44 @@ import lab_8.entities.User;
  * @author harshalneelkamal
  */
 public class AnalysisHelper {
-    
-    
-    public void userWithMostLikes(){
-        
-        
+
+    public void userWithMostLikes() {
+        Map<Integer, Integer> userLikecount = new HashMap<Integer, Integer>();
+        Map<Integer, User> users = DataStore.getInstance().getUsers();
+        for (User user : users.values()) {
+            for (Comment c : user.getComments()) {
+                int likes = 0;
+                if (userLikecount.containsKey(user.getId())) {
+                    likes = userLikecount.get(user.getId());
+                }
+                likes += c.getLikes();
+                userLikecount.put(user.getId(), likes);
+            }
+        }
+        int max = 0;
+        int maxId = 0;
+        for (int id : userLikecount.keySet()) {
+            if (userLikecount.get(id) > max) {
+                max = userLikecount.get(id);
+                maxId = id;
+            }
+        }
     }
-    
-    public void getFiveMostLikedComment(){
-        
-        
+
+    public void getFiveMostLikedComment() {
+        Map<Integer, Comment> comments = DataStore.getInstance().getComments();
+        List<Comment> commentList = new ArrayList<>(comments.values());
+        Collections.sort(commentList, new Comparator<Comment>() {
+            @Override
+            public int compare(Comment o1, Comment o2) {
+                //so as to get decending list
+                return o2.getLikes() - o1.getLikes();
+            }
+        });
+        System.out.println("5 most liked comments :");
+        for (int i = 0; i < commentList.size() && i < 5; i++) {
+            System.out.println(commentList.get(i));
+        }
     }
-    
+
 }
